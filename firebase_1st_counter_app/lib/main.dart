cb3dc0be8cb7ab4dc8f94502db9bfe6df3c9cb7d
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-07 00:52:21 +0000

    Update main.dart

--- firebase_1st_counter_app/lib/main.dart
+++ firebase_1st_counter_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart' as aa;
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async{
