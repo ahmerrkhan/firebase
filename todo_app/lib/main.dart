8d6e8a5099712450b322d8e2dd83e67c29264868
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-05-09 23:52:44 +0000

    Update main.dart

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo TODO',
       theme: ThemeData(
       ),
       home: landingPage(),
